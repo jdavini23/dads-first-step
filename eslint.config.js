5fc3eb7a8841d74922bbd48e1a02c624a9be7e55
Joe <joedavini23@yahoo.com>
2025-01-15 15:11:08 -0800

    fix: update dependencies and eslint configuration for build compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 // @ts-check
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import react from 'eslint-plugin-react';
+import globals from 'globals';
 
-export default tseslint.config(
+export default [
   {
     ignores: [
       '**/node_modules/**', 
@@ -24,15 +25,27 @@ export default tseslint.config(
     ]
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: { jsx: true }
+      }
+    },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       react
     },
     rules: {
+      ...tseslint.configs.recommended.rules,
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       'react-refresh/only-export-components': [
@@ -49,14 +62,6 @@ export default tseslint.config(
           varsIgnorePattern: '^_'
         }
       ]
-    },
-    languageOptions: {
-      parserOptions: {
-        project: './tsconfig.json',
-        ecmaFeatures: {
-          jsx: true
-        }
-      }
     }
   }
-);
+];
