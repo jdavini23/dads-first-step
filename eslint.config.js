2c0b4bcc186f474d11b37d93727d42f8d60e52c1
Joe <joedavini23@yahoo.com>
2025-01-15 14:38:43 -0800

    fix(eslint): Update ESLint configuration to use new flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,29 @@
 // @ts-check
 const eslint = require("eslint");
-const tseslint = require("typescript-eslint");
-const nextPlugin = require("@next/eslint-plugin-next");
+const tseslint = require("@typescript-eslint/eslint-plugin");
+const nextPlugin = require("eslint-config-next");
 
-module.exports = tseslint.config(
+module.exports = [
   {
+    files: ['**/*.ts', '**/*.tsx'],
     extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:react-hooks/recommended",
-      "next/core-web-vitals"
+      'plugin:@typescript-eslint/recommended',
+      'next/core-web-vitals'
     ],
-    plugins: ["@typescript-eslint"],
+    plugins: ['@typescript-eslint'],
     rules: {
-      // Customize rules here
-      "@typescript-eslint/no-unused-vars": "warn",
-      "react/no-unescaped-entities": "warn",
-      "no-console": "warn"
-    },
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/no-unescaped-entities': 'warn',
+      'no-console': 'warn'
+    }
+  },
+  {
     ignores: [
-      ".next/",
-      "node_modules/",
-      "dist/",
-      "build/",
-      "coverage/"
+      '.next/',
+      'node_modules/',
+      'dist/',
+      'build/',
+      'coverage/'
     ]
   }
-);
+];
